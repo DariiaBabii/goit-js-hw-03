b4dddb1b39c217e2a86a4260a3095819fa99dc46
dariia_babii <dariia.babii02@gmail.com>
2024-04-13 18:44:49 +0200

    arrays

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+function slugify(title) {
+  let slug;
+  let lowercase = title.toLowerCase();
+  let arrtitles = lowercase.split(" ");
+  slug = arrtitles.join("-");
+  return slug;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
